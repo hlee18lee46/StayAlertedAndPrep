a4e87d3b9f66e39e62215c54eb880de2e2dcb534
Han Lee <lee46@mail.usf.edu>
2024-10-18 15:36:20 -0400

    Initial Commit

---
+++ StayAlertedAndPrep/StayAlertedAndPrepApp.swift
@@ -0,0 +1,17 @@
+//
+//  StayAlertedAndPrepApp.swift
+//  StayAlertedAndPrep
+//
+//  Created by Han Lee on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct StayAlertedAndPrepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StayAlertedAndPrepTests/StayAlertedAndPrepTests.swift
@@ -0,0 +1,17 @@
+//
+//  StayAlertedAndPrepTests.swift
+//  StayAlertedAndPrepTests
+//
+//  Created by Han Lee on 10/18/24.
+//
+
+import Testing
+@testable import StayAlertedAndPrep
+
+struct StayAlertedAndPrepTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
